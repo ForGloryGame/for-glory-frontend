6cffc3bd80abb4d7d648f2739887be000bb4a16c
yqrashawn <namy.19@gmail.com>
2022-04-18 08:05:45 +0000

    Deploying to pages from @ ForGloryGame/for-glory-frontend@be85811c55104f2f61b5d422e40a2aaae70af0e4 🚀

--- js/compiled/404.js
+++ js/compiled/404.js
@@ -1,3 +1,3 @@
 (function(){
-'use strict';$APP.IT=function(){return new $APP.U(null,2,5,$APP.W,[$APP.kM,"Page not found"],null)};$APP.ed("404");$APP.HT={};$APP.cd.Lc();
+'use strict';$APP.UT=function(){return new $APP.U(null,2,5,$APP.W,[$APP.wM,"Page not found"],null)};$APP.ed("404");$APP.TT={};$APP.cd.Lc();
 }).call(this);
--- js/compiled/login.js
+++ js/compiled/login.js
@@ -1,3 +1,3 @@
 (function(){
-'use strict';$APP.GT=function(){return new $APP.U(null,1,5,$APP.W,[new $APP.v(null,2,[$APP.Eu,$APP.Ui,$APP.Fu,$APP.Ui],null)],null)};$APP.FT=function(){return new $APP.U(null,2,5,$APP.W,[$APP.kM,"login"],null)};$APP.ed("login");$APP.ET={};$APP.cd.Lc();
+'use strict';$APP.ST=function(){return new $APP.U(null,1,5,$APP.W,[new $APP.v(null,2,[$APP.Pu,$APP.Ui,$APP.Qu,$APP.Ui],null)],null)};$APP.RT=function(){return new $APP.U(null,2,5,$APP.W,[$APP.wM,"login"],null)};$APP.ed("login");$APP.QT={};$APP.cd.Lc();
 }).call(this);
