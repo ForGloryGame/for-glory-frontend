0ead28c6e64ade49c1559ee744184d780ecc6d8f
yqrashawn <namy.19@gmail.com>
2022-05-22 15:26:45 +0000

    Deploying to dev-app-pages from @ ForGloryGame/for-glory-frontend@c2aceb997fa8aa1dac981b0864afe631f394ff8b 🚀

--- js/compiled/500.js
+++ js/compiled/500.js
@@ -1,3 +1,3 @@
 (function(){
-'use strict';$APP.OG=function(){return new $APP.P(null,2,5,$APP.R,[$APP.lu,"Some thing went wrong"],null)};$APP.hd("500");$APP.PG={};$APP.Re();
+'use strict';$APP.WG=function(){return new $APP.P(null,2,5,$APP.R,[$APP.lu,"Some thing went wrong"],null)};$APP.hd("500");$APP.XG={};$APP.Re();
 }).call(this);
