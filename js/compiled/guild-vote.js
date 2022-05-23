8348e9f96fd1153804f9b7676c95bfb9529728da
yqrashawn <namy.19@gmail.com>
2022-05-23 14:20:07 +0000

    Deploying to dev-app-pages from @ ForGloryGame/for-glory-frontend@7560287654cae0e136f373a5ed31ae4dd9d4aa0d 🚀

--- js/compiled/guild-vote.js
+++ js/compiled/guild-vote.js
@@ -1,3 +1,3 @@
 (function(){
-'use strict';$APP.TG=function(){return new $APP.P(null,1,5,$APP.R,[new $APP.l(null,2,[$APP.bq,$APP.ki,$APP.TA,$APP.ki],null)],null)};$APP.UG=function(){return new $APP.P(null,2,5,$APP.R,[$APP.Mw,"vote"],null)};$APP.hd("guild-vote");$APP.VG={};$APP.Re();
+'use strict';$APP.LG=function(){return new $APP.P(null,1,5,$APP.R,[new $APP.l(null,2,[$APP.bq,$APP.ki,$APP.QA,$APP.ki],null)],null)};$APP.MG=function(){return new $APP.P(null,2,5,$APP.R,[$APP.Kw,"vote"],null)};$APP.hd("guild-vote");$APP.NG={};$APP.Re();
 }).call(this);
--- js/compiled/the-map.js
+++
@@ -1,3 +0,0 @@
-(function(){
-'use strict';$APP.u5=function(){return new $APP.P(null,1,5,$APP.R,[new $APP.l(null,2,[$APP.bq,$APP.ki,$APP.TA,$APP.ki],null)],null)};$APP.t5=function(){return new $APP.P(null,2,5,$APP.R,[$APP.SG,!0],null)};$APP.hd("the-map");$APP.s5={};$APP.Re();
-}).call(this);
