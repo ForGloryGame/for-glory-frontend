eaacdf70a03fa891f97bbe7df45570e16e3c9850
yqrashawn <namy.19@gmail.com>
2022-06-07 02:43:08 +0000

    Deploying to dev-app-pages from @ ForGloryGame/for-glory-frontend@282528ba007ede8921c055dd049ceac88f2c0f08 🚀

--- js/compiled/404.js
+++ js/compiled/404.js
@@ -1,3 +1,3 @@
 (function(){
-'use strict';$APP.SL=function(){return new $APP.J(null,2,5,$APP.K,[$APP.Ms,"Page not found"],null)};$APP.dd("404");$APP.RL={};$APP.Ke();
+'use strict';$APP.AM=function(){return new $APP.J(null,2,5,$APP.K,[$APP.Ms,"Page not found"],null)};$APP.dd("404");$APP.zM={};$APP.Ke();
 }).call(this);
--- js/compiled/500.js
+++ js/compiled/500.js
@@ -1,3 +1,3 @@
 (function(){
-'use strict';$APP.UL=function(){return new $APP.J(null,2,5,$APP.K,[$APP.Ms,"Some thing went wrong"],null)};$APP.dd("500");$APP.TL={};$APP.Ke();
+'use strict';$APP.CM=function(){return new $APP.J(null,2,5,$APP.K,[$APP.Ms,"Some thing went wrong"],null)};$APP.dd("500");$APP.BM={};$APP.Ke();
 }).call(this);
