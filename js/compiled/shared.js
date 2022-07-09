28ebf8a9002f8a04a983dbec568dae0959391550
yqrashawn <namy.19@gmail.com>
2022-07-09 06:25:41 +0000

    Deploying to dev-front-pages from @ ForGloryGame/for-glory-frontend@caf6032b1c77fd64f2049199e04dfeeaf2180ede 🚀

--- js/compiled/shared.js
+++
@@ -1,5 +0,0 @@
-
-var shadow$modules = {"uris":{"shared":["/js/compiled/shared.js"],"main":[]},"infos":{"shared":null,"main":["shared"]}};
-(function(){
-'use strict';$APP.aa();$APP.ba();
-}).call(this);
