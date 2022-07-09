aa547df695e62af55aba76d44475e908854cd26e
yqrashawn <namy.19@gmail.com>
2022-07-09 07:55:07 +0000

    Deploying to dev-app-pages from @ ForGloryGame/for-glory-frontend@9223b927a42eee0ba47204ff2cdfd6feb5803f0f 🚀

---
+++ js/compiled/map.js
@@ -0,0 +1,3 @@
+(function(){
+'use strict';$APP.LZ=function(){return new $APP.H(null,1,5,$APP.I,[new $APP.g(null,2,[$APP.Cp,$APP.Zh,$APP.bB,$APP.Zh],null)],null)};$APP.KZ=function(){return new $APP.H(null,2,5,$APP.I,[$APP.nH,!0],null)};$APP.cd("map");$APP.JZ={};$APP.Je();
+}).call(this);
