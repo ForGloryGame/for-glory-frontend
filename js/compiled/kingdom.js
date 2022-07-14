4f5136ef617ac79927876cb3f45420363953dd06
yqrashawn <namy.19@gmail.com>
2022-07-14 13:03:22 +0000

    Deploying to dev-app-pages from @ ForGloryGame/for-glory-frontend@aa28c8bbcd50d1e4b8e5e350b9d50b481b132815 🚀

--- js/compiled/kingdom.js
+++ js/compiled/kingdom.js
@@ -1,3 +1,3 @@
 (function(){
-'use strict';$APP.yH=function(){return new $APP.H(null,1,5,$APP.I,[new $APP.g(null,2,[$APP.Cp,$APP.Zh,$APP.fB,$APP.Zh],null)],null)};$APP.zH=function(){return new $APP.H(null,2,5,$APP.I,[$APP.cG,!0],null)};$APP.cd("kingdom");$APP.AH={};$APP.Je();
+'use strict';$APP.yH=function(){return new $APP.H(null,1,5,$APP.I,[new $APP.g(null,2,[$APP.Cp,$APP.Zh,$APP.gB,$APP.Zh],null)],null)};$APP.zH=function(){return new $APP.H(null,2,5,$APP.I,[$APP.cG,!0],null)};$APP.cd("kingdom");$APP.AH={};$APP.Je();
 }).call(this);
--- js/compiled/map.js
+++ js/compiled/map.js
@@ -1,3 +1,3 @@
 (function(){
-'use strict';var E_;$APP.d_=function(){return new $APP.H(null,1,5,$APP.I,[new $APP.g(null,2,[$APP.Cp,function(){$APP.jv(E_,!0);return $APP.jv($APP.YD,!1)},$APP.fB,function(){$APP.jv($APP.YD,!0);return $APP.jv(E_,!1)}],null)],null)};$APP.c_=function(){return new $APP.H(null,2,5,$APP.I,[$APP.xH,!0],null)};E_=$APP.K("map-in");$APP.cd("map");$APP.b_={};$APP.Je();
+'use strict';var E_,F_;$APP.d_=function(){return new $APP.H(null,1,5,$APP.I,[new $APP.g(null,2,[$APP.Cp,function(){$APP.jv(E_,!0);return $APP.jv(F_,!1)},$APP.gB,function(){$APP.jv(F_,!0);return $APP.jv(E_,!1)}],null)],null)};$APP.c_=function(){return new $APP.H(null,2,5,$APP.I,[$APP.xH,!0],null)};E_=$APP.K("map-in");F_=$APP.K("map-out");$APP.cd("map");$APP.b_={};$APP.Je();
 }).call(this);
