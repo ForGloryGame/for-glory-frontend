27be8a38909287d8c995aeccf99b6c20324f47d5
yqrashawn <namy.19@gmail.com>
2022-07-14 23:25:15 +0800

    feat: connect btn clicked style

--- src/fgl/app/ui/connect_btn.cljs
+++ src/fgl/app/ui/connect_btn.cljs
@@ -1,6 +1,6 @@
 (ns fgl.app.ui.connect-btn
+  #_{:clj-kondo/ignore [:unused-namespace]}
   (:require
-   [fgl.app.ui.btn :as btn]
    [fgl.utils :refer [shorten-addr]]
    [fgl.wallet.core :as w]
    [lambdaisland.glogi :as log]
@@ -31,13 +31,19 @@
     (fn [_]
       (and target-chain-id (w/init! {:target-chain-id target-chain-id})))
     :reagent-render
-    (fn []
-      (let [[text name disabled on-click] @(rf/subscribe [::text])]
-        [:button.block.bg-C25a1b7.h-full.px-8.fb.text-xl.py-1
-         {:style {:textShadow "1.364px 1.463px 1px rgba(0, 0, 0, 0.64)"
-                  :textAlign "justifyCenter"}
-          :disabled disabled
-          :on-click on-click
-          :id       "connect-btn"
-          :name     name}
-         text]))}))
+    (let [mouse-down                    (r/atom false)]
+      (fn []
+        (let [[text name disabled on-click] @(rf/subscribe [::text])]
+          [:button.block.bg-C25a1b7.h-full.px-8.fb.text-xl.py-1.border
+           {:style       {:borderColor (if @mouse-down "rgb(94, 203, 231)" "#25a1b7")
+                          :textShadow "1.364px 1.463px 1px rgba(0, 0, 0, 0.64)"
+                          :backgroundColor (when @mouse-down "rgb(49, 115, 127)")
+                          :boxShadow (when @mouse-down "6.82px 7.314px 8px 0px rgba(0, 0, 0, 0.17),1.03px 1.714px 0px 0px rgba(45, 45, 45, 0.5)")
+                          :textAlign  "justifyCenter"}
+            :disabled    disabled
+            :on-click    on-click
+            :onMouseDown #(reset! mouse-down true)
+            :onMouseUp   #(reset! mouse-down false)
+            :id          "connect-btn"
+            :name        name}
+           text])))}))
